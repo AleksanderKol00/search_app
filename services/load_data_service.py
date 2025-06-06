f1803a37564960ddbd2879da52a93be8d961fc3e
alex <kolowskialeksander@gmail.com>
2025-06-06 22:11:15 +0200

    Refactor SearchService

--- services/load_data_service.py
+++ services/load_data_service.py
@@ -5,15 +5,15 @@ log = logging.getLogger(__name__)
 
 
 def get_data_from_file(file_path: str) -> list[Optional[int]]:
-    log.info(f"Loading data from file path: {file_path}")
+    log.info(f"Loading data from file path: {file_path}.")
     try:
         with open(file_path, 'r') as file:
             file_data = [int(line.strip()) for line in file]
     except FileNotFoundError:
-        log.error(f"File {file_path} not found")
+        log.error(f"File {file_path} not found.")
         raise
     except Exception as e:
-        log.error(f"Failed to load data from file {file_path}: {e}")
+        log.error(f"Failed to load data from file {file_path}: {e}.")
         raise
-    log.info(f"Successfully loaded data from file {file_path}")
+    log.info(f"Successfully loaded data from file {file_path}.")
     return file_data
--- services/search_service.py
+++ services/search_service.py
@@ -1,6 +1,5 @@
 import logging
 from bisect import bisect_left
-from turtledemo.penrose import inflatedart
 from typing import Optional
 
 from pydantic import BaseModel
@@ -22,8 +21,33 @@ class SearchService:
     def __init__(self, data: list[int]) -> None:
         self.data = data
 
+    def _get_min_possible_value(self, value) -> int:
+        return value * (1 - self.CONFORMATION_LEVEL)
+
+    def _get_max_possible_value(self, value) -> int:
+        return value * (1 + self.CONFORMATION_LEVEL)
+
+    def _get_nearby_acceptable_index(self, value, index) -> Optional[int]:
+        min_possible = self._get_min_possible_value(value)
+        max_possible_value = self._get_max_possible_value(value)
+        # Check left value
+        if index > 0:
+            if min_possible <= self.data[index - 1] <= max_possible_value:
+                log.debug(
+                    f"Left value matches the wanted value, index: {index - 1}, value: {self.data[index - 1]}.")
+                return index - 1
+
+        # Check right value
+        if index < len(self.data):
+            if min_possible <= self.data[index] <= max_possible_value:
+                log.debug(
+                    f"Returned index matches the wanted value: {index},  index: {index}, value: {self.data[index]}.")
+                return index
+
+        return None
+
     def _get_index(self, value: int) -> Optional[int]:
-        log.debug(f"Getting index for value: {value}")
+        log.debug(f"Getting index for value: {value}.")
         index = bisect_left(self.data, value)
         log.debug(f"bisect left index: {index}")
 
@@ -32,28 +56,12 @@ class SearchService:
             log.debug(f"Found value with exact index: {index}")
             return index
 
-        log.debug(f"Didn't find value with exact index: {index}. Trying to find acceptable value")
-        min_possible = value * (1 - self.CONFORMATION_LEVEL)
-        max_possible_value = value * (1 + self.CONFORMATION_LEVEL)
-        # Check left value
-        if index > 0:
-            if min_possible <= self.data[index - 1] <= max_possible_value:
-                log.debug(
-                    f"Left value matches the wanted value, index: {index - 1}, value: {self.data[index - 1]}")
-                return index - 1
-
-        # Check right value
-        if index < len(self.data):
-            if min_possible <= self.data[index] <= max_possible_value:
-                log.debug(
-                    f"Returned index matches the wanted value: {index},  index: {index}, value: {self.data[index]}")
-                return index
-
-        return None
+        log.debug(f"Didn't find value with exact index: {index}. Trying to find acceptable value.")
+        return self._get_nearby_acceptable_index(value, index)
 
     def find_item(self, value: int) -> SearchItem:
         index = self._get_index(value)
         msg = ERROR_MSG if index is None else ""
         value = self.data[index] if index is not None else value
-        log.debug(f"Return search item with index: {index}, value: {value}, message: {msg}")
+        log.debug(f"Return search item with index: {index}, value: {value}, message: {msg}.")
         return SearchItem(index=index, value=value, message=msg)
