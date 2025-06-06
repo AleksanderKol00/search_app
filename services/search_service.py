1b1e89a497298b0c20bece4c2bbf577bf705c8ea
alex <kolowskialeksander@gmail.com>
2025-06-06 20:16:30 +0200

    Adjust search algorithm to be able to find index of acceptable value

--- services/search_service.py
+++ services/search_service.py
@@ -1,7 +1,7 @@
 from bisect import bisect_left
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validate_email
 
 from routes.static import ERROR_MSG
 
@@ -13,13 +13,30 @@ class SearchItem(BaseModel):
 
 
 class SearchService:
+    CONFORMATION_LEVEL = 0.1
+
     def __init__(self, data: list[int]) -> None:
         self.data = data
 
     def _get_index(self, value: int) -> Optional[int]:
         index = bisect_left(self.data, value)
+
+        # Check if there's value for given index
         if index != len(self.data) and self.data[index] == value:
             return index
+
+        min_possible = value * (1 - self.CONFORMATION_LEVEL)
+        max_possible_value = value * (1 + self.CONFORMATION_LEVEL)
+        # Check left value
+        if index > 0:
+            if min_possible <= self.data[index - 1] <= max_possible_value:
+                return index - 1
+
+        # Check right value
+        if index < len(self.data):
+            if min_possible <= self.data[index] <= max_possible_value:
+                return index
+
         return None
 
     def find_item(self, value: int) -> SearchItem:
--- tests/unit/test_search_service.py
+++ tests/unit/test_search_service.py
@@ -5,7 +5,8 @@ from services.search_service import SearchService
 
 @pytest.mark.parametrize("data, value, exp_index", [
     ([0, 10, 20, 100], 100, 3),
-    ([], 100, None)
+    ([], 100, None),
+    ([1100, 1200], 1150, 0)
 ])
 def test_get_index(data, value, exp_index):
-    assert exp_index == SearchService(data).get_index(value)
+    assert exp_index == SearchService(data).find_item(value).index
